c36d391812e39bffe18359d4168457e985a53017
Eren AVAR <eren.avar@hotmail.com>
2025-02-07 20:45:43 +0300

    add google fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyle";
 
 export default function App() {
-  return <div>Hello World</div>;
+  return <GlobalStyles />;
 }
