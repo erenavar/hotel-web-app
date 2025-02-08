8753d57d5245d55ffb6ea77ff8edbd1b8dc6a9e9
Eren AVAR <eren.avar@hotmail.com>
2025-02-08 12:55:45 +0300

    make app.js schema but not complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyle";
+import Row from "./ui/Row";
 
 export default function App() {
-  return <GlobalStyles />;
+  return (
+    <>
+      <GlobalStyles />
+      <Row type="horizontal">
+        <div>
+          <h1>The Hotel</h1>
+        </div>
+        <div>Check-in / Check-out</div>
+      </Row>
+      <Row type="vertical">
+        <h1>Form</h1>
+        <div></div>
+      </Row>
+    </>
+  );
 }
